c3ab99993a384c69f487b4c8ca3ee12b46ee2356
Geoffjef <68147991+Geoffjef@users.noreply.github.com>
2020-07-18 12:08:57 +0100

    these changes are not yet done but i want to save them some things are just to ssee if code works

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
 namespace SpriteKind {
     export const Princess = SpriteKind.create()
 }
@@ -117,8 +122,23 @@ f f f f f f f f f f f f f f f f
 9 9 . . . . . . . . . . . . 9 9 
 `
 }
+scene.onOverlapTile(SpriteKind.Projectile, myTiles.tile5, function (sprite, location) {
+    if (barrel_detection_thing == 2) {
+        projectile4.ay = 350
+        pause(500)
+        projectile4.ay = 0
+        if (projectile4.vx == 150) {
+            projectile4.vx = -150
+        } else {
+            projectile4.vx = 150
+        }
+    }
+})
 info.onCountdownEnd(function () {
-    projectile4 = sprites.createProjectileFromSide(img`
+    barrel_detection_thing = Math.randomRange(1, 2)
+    if (barrel_detection_thing == 1) {
+        scene.cameraFollowSprite(projectile4)
+        projectile4 = sprites.createProjectileFromSide(img`
 . . e f e e e e e e e e f e . . 
 . e e f e e e e e e e e f e e . 
 e e e f e e e e e e e e f e e e 
@@ -135,8 +155,30 @@ e e e f e e e e e e e e f e e e
 e e e f e e e e e e e e f e e e 
 . e e f e e e e e e e e f e e . 
 . . e f e e e e e e e e f e . . 
-`, 75, 0)
-    tiles.placeOnTile(projectile4, tiles.getTileLocation(0, 4))
+`, 0, 150)
+        tiles.placeOnTile(projectile4, tiles.getTileLocation(Math.randomRange(0, 29), 4))
+    } else {
+        scene.cameraFollowSprite(projectile4)
+        projectile4 = sprites.createProjectileFromSide(img`
+. . e f e e e e e e e e f e . . 
+. e e f e e e e e e e e f e e . 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+. e e f e e e e e e e e f e e . 
+. . e f e e e e e e e e f e . . 
+`, 150, 0)
+        animation.attachAnimation(projectile4, animation2)
+    }
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mario.vy == 0) {
@@ -144,6 +186,8 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 let projectile4: Sprite = null
+let barrel_detection_thing = 0
+let animation2: animation.Animation = null
 let mario: Sprite = null
 tiles.setTilemap(tiles.createTilemap(
             hex`1e001e00000000000000030000000000000000000000000000000000000000000000000000000000010105050000000000000000000000000000000000000000000000000000000005050000000000000000000000000000000000000000020000000000000005050000000000000000000000000000000000000000000000000000000005050000000000000000000000000000000000000000010101010101010101010101010101010101010101010101010505010101000000000000000000000000000000000000000000000000000505000000000000000000000000000000000000000000000000000000000505000000000000000000000000000000000000000000000000000000000505000000000000000000000000000000000000000101010101010101010101010101010101050501010101010101010101010000000000000000000000000000000000050500000000000000000000000000000000000000000000000000000000050500000000000000000000000000000000000000000000000000000000050500000000000000000000000000000000000000000000000000010101010101010101010100000000000000000000000000000000000000000000000000000000000001010101010101010101010101010505010101000000000000000000000000000000000000000000000000000505000000000000000000000000000000000000000000000000000000000505000000000000000000000000000000000000000000000000000000000505000000000000000000000000000000000000000000000001010101010101010101010101050501010101010101010101010101010100000000000000000000000000050500000000000000000000000000000000000000000000000000000000050500000000000000000000000000000000000000000000000000000000050500000000000000000000000000000000000000000000000000010101010101010101010100000000000000000000000000000000000000000000000000000000000001010101010101010101010101010505010101000000000000000000000000000000000000000000000000000505000000040000000000000000000000000000000000000000000000000505000000000000000000000000000001010101010101010101010101010101010101010101010101010101010100000000000000000000000000000000000000`,
@@ -182,6 +226,7 @@ tiles.setTilemap(tiles.createTilemap(
             [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5],
             TileScale.Sixteen
         ))
+let level = 1
 mario = sprites.create(img`
 . . . . . 2 2 2 2 2 2 . . . . . 
 . . . . 2 2 2 2 2 2 2 2 2 2 . . 
@@ -204,3 +249,53 @@ controller.moveSprite(mario, 100, 0)
 mario.ay = 300
 scene.cameraFollowSprite(mario)
 tiles.placeOnRandomTile(mario, myTiles.tile4)
+while (level == 1) {
+    info.startCountdown(5)
+    pause(5000)
+    animation2 = animation.createAnimation(ActionKind.Walking, 100)
+    animation2.addAnimationFrame(img`
+. . e f e e e e e e e e f e . . 
+. e e f e e e e e e e e f e e . 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+e e e f e e e e e e e e f e e e 
+. e e f e e e e e e e e f e e . 
+. . e f e e e e e e e e f e . . 
+`)
+    animation2.addAnimationFrame(img`
+. . e e e e e e e e e e e e . . 
+. e e e e e e e e e e e e e e . 
+e e e e e e e e e e e e e e e e 
+f f f f f f f f f f f f f f f f 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+f f f f f f f f f f f f f f f f 
+e e e e e e e e e e e e e e e e 
+. e e e e e e e e e e e e e e . 
+. . e e e e e e e e e e e e . . 
+`)
+}
+forever(function () {
+    if (barrel_detection_thing == 1) {
+        if (projectile4.tileKindAt(TileDirection.Bottom, myTiles.tile1)) {
+            projectile4.setFlag(SpriteFlag.Ghost, true)
+            pause(500)
+            projectile4.setFlag(SpriteFlag.Ghost, false)
+        }
+    }
+})
